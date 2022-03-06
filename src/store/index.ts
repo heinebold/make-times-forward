40d5735745d516a5f99fe8c66de95cc3b8da754b
heinebold <mb@matthias-brueckner.de>
2022-03-06 11:59:34 +0100

    Add the schedule to the actual view and store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import type { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
 import dayjs, { Dayjs } from "dayjs";
+import type { TimeSlot } from "@/model/TimeSlot";
 
 export interface State {
   clock: () => Dayjs;
   time: Dayjs;
   use12hTime: boolean;
+  schedule: TimeSlot[];
 }
 
 // define injection key
@@ -15,14 +17,26 @@ export function useStore() {
   return baseUseStore(key);
 }
 
+function readStoredSchedule() {
+  try {
+    return JSON.parse(localStorage.getItem("schedule") || "", (k, v) => {
+      return k === "start" || k === "end" ? dayjs(v) : v;
+    });
+  } catch (e) {
+    return [];
+  }
+}
+
 export const store = createStore<State>({
   strict: process.env.NODE_ENV !== "production",
   state() {
     const clock = dayjs;
+
     return {
       clock,
       time: clock(),
       use12hTime: localStorage.getItem("timeDisplayMode") === "12h",
+      schedule: readStoredSchedule(),
     };
   },
   mutations: {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,14 @@
 <template>
-  This app is currently useless because nobody implemented any functionality
+  <current-schedule-panel :items="schedule" />
+  <full-schedule-panel :items="schedule" />
 </template>
+
+<script setup lang="ts">
+import CurrentSchedulePanel from "@/components/CurrentSchedulePanel.vue";
+import FullSchedulePanel from "@/components/FullSchedulePanel.vue";
+import { useStore } from "@/store";
+import { computed } from "vue";
+
+const store = useStore();
+const schedule = computed(() => store.state.schedule);
+</script>
