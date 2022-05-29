acaeb02eff3a8cf4392128acdd566e4cf90edaf6
heinebold <mb@matthias-brueckner.de>
2022-05-29 15:03:27 +0200

    Prefer setInterval over masses of setTimeout in clock logic

--- src/composables/clock.ts
+++ src/composables/clock.ts
@@ -3,23 +3,47 @@ import { readonly, ref } from "vue";
 
 type ClockFunction = () => Dayjs;
 
-const dummyClock = () => dayjs("1970-01-01T13:37:42");
+const dummyClock = () => dayjs("1970-01-01T13:37:42.999Z");
 let appClock: ClockFunction = dummyClock;
 const time = ref(appClock());
 
-let tickerTimeout: NodeJS.Timeout;
+let timer: NodeJS.Timeout;
+function clearTimer() {
+  clearInterval(timer);
+  clearTimeout(timer);
+}
+
+function startClock() {
+  const millisecond = updateTime();
+  adjustClock(millisecond);
+}
 
 function tickClock() {
-  time.value = appClock();
-  const ms = time.value.millisecond();
-  const timeout = (ms > 900 ? 2000 : 1000) - ms;
-  tickerTimeout = setTimeout(tickClock, timeout);
+  const millisecond = updateTime();
+  if (millisecond > 50 && millisecond < 950) {
+    adjustClock(millisecond);
+  }
+}
+
+function updateTime() {
+  return (time.value = appClock()).millisecond();
+}
+
+function adjustClock(millisecond: number) {
+  const adjustTimeout = (millisecond > 900 ? 2000 : 1000) - millisecond;
+  clearTimer();
+  timer = setTimeout(restartClockwork, adjustTimeout);
+}
+
+function restartClockwork() {
+  clearTimer();
+  timer = setInterval(tickClock, 1000);
+  updateTime();
 }
 
 export function setAppClock(newClock: ClockFunction) {
-  clearTimeout(tickerTimeout);
   appClock = newClock;
-  tickClock();
+  startClock();
 }
 
 export function useClock() {
