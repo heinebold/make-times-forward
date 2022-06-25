9d03d2fe9a9bf87bd35865a1fdfdb6c63218f017
heinebold <mb@matthias-brueckner.de>
2022-06-25 14:52:21 +0100

    Add test for ScheduleItem

---
+++ src/components/__tests__/ScheduleItem.spec.ts
@@ -0,0 +1,48 @@
+import { describe, it, expect, vi, beforeAll, afterAll } from "vitest";
+
+import { mount, VueWrapper } from "@vue/test-utils";
+import ScheduleItem from "../ScheduleItem.vue";
+import dayjs from "dayjs";
+import { createTestingPinia } from "@pinia/testing";
+
+describe("The ScheduleItem component", () => {
+  const now = dayjs("2000-01-01T13:37:42Z").toDate();
+  const start = dayjs("2000-01-01T13:40Z");
+  const end = dayjs("2000-01-01T16:20Z");
+  const title = "Some event";
+
+  let wrapper: VueWrapper;
+
+  beforeAll(() => {
+    vi.useFakeTimers({ now });
+
+    wrapper = mount(ScheduleItem, {
+      props: { start, end, title },
+      global: {
+        plugins: [
+          createTestingPinia({
+            createSpy: vi.fn,
+          }),
+        ],
+      },
+    });
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it("Shows the title", () => {
+    expect(wrapper.find(".title").text()).toEqual(title);
+  });
+
+  it("Shows start info", () => {
+    expect(wrapper.html()).toContain(start.hour());
+    expect(wrapper.html()).toContain(start.minute());
+  });
+
+  it("Shows end info", () => {
+    expect(wrapper.html()).toContain(end.hour());
+    expect(wrapper.html()).toContain(end.minute());
+  });
+});
