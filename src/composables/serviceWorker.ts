4fc8d219261c81e1d687b5a2bca92260f7cc406b
heinebold <mb@matthias-brueckner.de>
2022-10-22 18:52:42 +0200

    Make periodic checks for new versions of the service worker

--- src/composables/serviceWorker.ts
+++ src/composables/serviceWorker.ts
@@ -12,12 +12,43 @@ function updateApp(reloadPage?: boolean) {
   return updateFunction(reloadPage);
 }
 
+let updateInterval: ReturnType<typeof setInterval> | null = null;
+
+function enableUpdateCheck(swUrl: string, intervalSeconds: number) {
+  if (!registration || !swUrl) {
+    return;
+  }
+  if (updateInterval !== null) {
+    clearInterval(updateInterval);
+  }
+  updateInterval = setInterval(async () => {
+    if (!navigator?.onLine || !("connection" in navigator)) {
+      return;
+    }
+
+    if (registration?.installing) {
+      return;
+    }
+
+    const resp = await fetch(swUrl, {
+      cache: "no-store",
+      headers: {
+        "cache-control": "no-cache",
+      },
+    });
+    if (resp?.status === 200) {
+      await registration?.update().catch(console.debug);
+    }
+  }, 1000 * Math.max(10, intervalSeconds));
+}
+
 export const useServiceWorker = () => {
   if (updateAvailable === undefined) {
     const { needRefresh, updateServiceWorker } = useRegisterSW({
       immediate: false,
       onRegisteredSW(url, reg) {
         registration = reg;
+        enableUpdateCheck(url, 5 * 60);
       },
       onOfflineReady() {
         if (!registration?.active) {
@@ -29,7 +60,7 @@ export const useServiceWorker = () => {
         messageSW(registration.active, {
           type: "CACHE_URLS",
           payload: { urlsToCache: initiallyLoadedResources },
-        }).catch(console.log);
+        }).catch(console.debug);
       },
     });
     updateAvailable = needRefresh;
