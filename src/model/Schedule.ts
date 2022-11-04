18446c3c608b527c370601727efa59ce85970d1e
heinebold <mb@matthias-brueckner.de>
2022-11-04 11:09:51 +0100

    Refactor for readability

--- src/model/Schedule.ts
+++ src/model/Schedule.ts
@@ -9,26 +9,32 @@ const Schedule = new t.Type<Schedule, object[], unknown>(
   "Schedule",
   (value: unknown): value is Schedule =>
     Array.isArray(value) && value.every(TimeSlot.is),
-  (value, context) => {
-    if (!Array.isArray(value)) {
-      return t.failure(value, context, "Schedule is not an array");
-    }
-    const timeSlots = value
-      .map(ReadTimeSlot.pipe(TimeSlot).decode)
-      .map((v) => {
-        if (import.meta.env.MODE !== "production" && isLeft(v)) {
-          v.left.forEach((e) =>
-            console.warn(e.message, JSON.stringify(e.value))
-          );
-        }
-        return v;
-      })
-      .filter(isRight)
-      .map((r) => r.right);
-    return t.success(timeSlots);
-  },
+  (value, context) =>
+    Array.isArray(value)
+      ? t.success(decodeValidTimeSlots(value))
+      : t.failure(value, context, "Schedule is not an array"),
   (value) => value.map(WriteTimeSlot.encode)
 );
+
+export function parseSchedule(text: string): Schedule | null {
+  const jsonResult: Either<string, unknown> = tryParse(text);
+  if (isLeft(jsonResult)) {
+    if (import.meta.env.MODE !== "production") {
+      console.warn(jsonResult.left);
+    }
+    return null;
+  }
+  const result = Schedule.decode(jsonResult.right);
+  logLeft(result);
+  return isRight(result) ? result.right : null;
+}
+
+export function stringifySchedule(
+  schedule: Schedule,
+  space?: string | number
+): string {
+  return JSON.stringify(Schedule.encode(schedule), null, space);
+}
 
 function tryParse(text: string): Either<string, unknown> {
   try {
@@ -38,26 +44,16 @@ function tryParse(text: string): Either<string, unknown> {
   }
 }
 
-export function parseSchedule(text: string): Schedule | null {
-  const jsonResult: Either<string, unknown> = tryParse(text);
-  if (isLeft(jsonResult)) {
-    return null;
-  }
-  const result = Schedule.decode(jsonResult.right);
-  if (isRight(result)) {
-    return result.right;
-  }
-  if (import.meta.env.MODE !== "production") {
-    result.left.forEach((e) =>
-      console.warn(e.message, JSON.stringify(e.value))
-    );
-  }
-  return null;
-}
+const decodeValidTimeSlots = (items: unknown[]) =>
+  items
+    .map(ReadTimeSlot.pipe(TimeSlot).decode)
+    .map(logLeft)
+    .filter(isRight)
+    .map((r) => r.right);
 
-export function stringifySchedule(
-  schedule: Schedule,
-  space?: string | number
-): string {
-  return JSON.stringify(Schedule.encode(schedule), null, space);
+function logLeft<T>(v: t.Validation<T>): t.Validation<T> {
+  if (import.meta.env.MODE !== "production" && isLeft(v)) {
+    v.left.forEach((e) => console.warn(e.message, JSON.stringify(e.value)));
+  }
+  return v;
 }
